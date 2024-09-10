fc24c24092c3efc1807269226d099db69d11d85e
marconys <marconysbatera@gmail.com>
2024-09-10 20:11:11 -0300

    Criando Seed para cadastrar administrador padrão

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -13,6 +13,18 @@ public class DbContexto : DbContext
     }
     public DbSet<Administrador> Administradores { get; set; } = default!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador {
+                Id = 1,
+                Email = "administrador@teste.com",
+                Senha = "1234",
+                Perfil = "Adm"
+            }
+        );
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
